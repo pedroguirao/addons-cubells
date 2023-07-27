2969b3aac0d464f1277c59fa7eee695d35e69256
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-27 13:13:30 +0200

    depends

--- purchase_price_update/models/purchase.py
+++ purchase_price_update/models/purchase.py
@@ -54,17 +54,18 @@ class PurchaseOrderLine(models.Model):
 
     date_price = fields.Date('Date price', default=datetime.now().date())
 
-    @api.onchange('product_qty', 'product_uom')
-    def _onchange_quantity(self):
-        res = super(PurchaseOrderLine, self)._onchange_quantity()
-        if not self.product_id:
-            return
+# Comentado 27/07, da error en línea date= (existe en estándar "purchase" 16, y ahora discount es purchase_discount no como en v11)
+#    @api.onchange('product_qty', 'product_uom')
+    #    def _onchange_quantity(self):
+    #    res = super(PurchaseOrderLine, self)._onchange_quantity()
+    #    if not self.product_id:
+    #        return
 
-        seller = self.product_id._select_seller(
-            partner_id=self.partner_id,
-            quantity=self.product_qty,
-            date=self.order_id.date_order and self.order_id.date_order[:10],
-            uom_id=self.product_uom)
-        if seller:
-            self.discount = seller.discount
-        return res
+    #    seller = self.product_id._select_seller(
+    #        partner_id=self.partner_id,
+    #        quantity=self.product_qty,
+    #        date=self.order_id.date_order and self.order_id.date_order[:10],
+    #        uom_id=self.product_uom)
+    #    if seller:
+    #        self.discount = seller.discount
+#    return res
