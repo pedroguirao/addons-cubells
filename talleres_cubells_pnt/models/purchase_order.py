83ea20039489a8cb82673671b36c46c21dfa4661
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-07-28 17:19:27 +0200

    r2

--- talleres_cubells_pnt/models/purchase_order.py
+++ talleres_cubells_pnt/models/purchase_order.py
@@ -4,5 +4,4 @@ from odoo import models, api, fields
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-
     date_order_pnt = fields.Datetime('Fecha pedido', related='date_order', store=True)
